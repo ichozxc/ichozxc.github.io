e91ff5d478fbeece02f1679e6a2348bc1a7ccaf3
isaac escobar <xzxzcdsa@gmail.com>
2021-10-07 14:20:08 -0500

    Delete index_files directory

--- index_files/U-mEmergencyAlert.js
+++
@@ -1,6 +0,0 @@
-document.write("\x3cdiv class\x3d\x22feedburnerFeedBlock\x22 id\x3d\x22U-mEmergencyAlertlskstmr4dmf3hv0m5csiakdse0\x22\x3e");
-document.write("\x3cul\x3e");
-document.write("\x3cdiv\x3eThe feed providing these headlines is not available.\x3c/div\x3e");
-document.write("\x3c/ul\x3e");
-document.write(" \x3cdiv id\x3d\x22creditfooter\x22\x3e\x3ca href\x3d\x22https://feedburner.google.com\x22 target\x3d\x22_blank\x22\x3e\x3cimg src\x3d\x22//feedburner.google.com/fb/images/buzzboost-pwrd.gif\x22 alt\x3d\x22Headlines by FeedBurner\x22 style\x3d\x22border:0\x22/\x3e\x3c/a\x3e\x3c/div\x3e");
-document.write("\x3c/div\x3e");
